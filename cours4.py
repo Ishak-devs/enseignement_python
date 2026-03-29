a6580a500ead91b2b3964266317fb38a912f804b
ishak <i.kouici@insta.fr>
2026-03-29 18:23:36 +0200

    on init la db

--- cours4.py
+++ cours4.py
@@ -1,3 +1,23 @@
 import psycopg2
+from fastapi import HTTPException
 
-psycopg2.connect(host="ishak", user="ishakuser", password="ishak2301", dbname="ma_db")
+def connexion_db():
+    conn = None
+
+    try:
+
+        conn = psycopg2.connect(host="ishak", user="ishakuser", password="ishak2301", dbname="ma_db")
+
+        cursor = conn.cursor()
+
+        query_ = "SELECT * FROM candidats"
+
+        cursor.execute(query_)
+        resultat = cursor.fetchall()
+        return resultat
+    except Exception:
+        raise HTTPException(status_code=500)
+
+    finally:
+        if conn:
+            conn.close()
